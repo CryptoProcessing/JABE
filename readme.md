6000cbcd79dcd3bef3cf8c3d00ff264286966e93
Leonov <a_leonovv@mail.ru>
2017-12-28 23:46:46 +0300

    deployment

--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-Create DB and user
+##Create DB and user
 ```bash
 mysql -u root -p
 ```
@@ -19,6 +19,13 @@ GRANT ALL ON jabe.* TO jabe@localhost IDENTIFIED BY 'jabe';
 GRANT ALL ON jabe_test.* TO jabe@localhost IDENTIFIED BY 'jabe';
 ```
 
+## Virtual environment 
+```bash
+virtualenv -p python3 venv
+source venv/bin/activate
+pip3 install -r requirements.txt
+```
+
 
 ## Миграции
 
@@ -32,3 +39,46 @@ python manage.py db init
 python manage.py db migrate
 python manage.py db upgrade
 ```
+
+## start gunicorn
+если еще не установлен supervisor то
+```bash
+apt-get install supervisor
+```
+скопровать в /etc/supervisor/conf.d/
+конфиг Gunicorn из папки extra/etc/supervisor/conf.d/jabe.conf
+
+команды для supervisor
+```bash
+supervisorctl reread
+supervisorctl update
+supervisorctl status jabe
+supervisorctl restart jabe
+```
+проверка
+ps xa | grep gunicorn
+
+
+
+## start celery deamon
+
+https://github.com/celery/celery/tree/master/extra/generic-init.d
+
+```bash
+ Скопировать celeryd в /etc/init.d/celeryd
+$ sudo chmod 755 /etc/init.d/celeryd
+$ sudo chown root:root /etc/init.d/celeryd
+
+Скопировать конфигурационный файл в /etc/default/celeryd
+
+Запуск
+$ sudo /etc/init.d/celeryd start
+Статус
+$ sudo /etc/init.d/celeryd status
+Остановка
+$ sudo /etc/init.
+```
+##nginx
+
+```bash
+ln -s /home/melaman/JABE/nginx_jabe.conf /etc/nginx/sites-available/nginx_jabe.conf
---
+++ tests/test_block_checker.py
@@ -0,0 +1,32 @@
+import io
+import unittest
+from unittest.mock import patch
+from tests.base import BaseTestCase
+from pycoin.block import Block as BlockObject
+from pycoin.serialize import h2b
+from controllers.api_controller import block_checker
+
+
+class TestBlockChecker(BaseTestCase):
+
+    def setUp(self):
+        super(TestBlockChecker, self).setUp()
+
+        block_hash = '010000006f187fddd5e28aa1b4065daa5d9eae0c487094fb20cf97ca02b81c84000000005b7b25b51797f8319' \
+                     '2f9fd2c3871bfb27570a7d6b56d3a50760613d1a2fc1aeeab346849ffff001d36d95071010100000001000000' \
+                     '0000000000000000000000000000000000000000000000000000000000ffffffff0704ffff001d0120fffffff' \
+                     'f0100f2052a0100000043410408ab2f56361f83064e4ce51acc291fb57c2cbcdb1d6562f6278c43a1406b548fd' \
+                     '6cefc11bcc29eb620d5861cb9ed69dc39f2422f54b06a8af4f78c8276cfdc6bac00000000'
+
+        block_data = h2b(block_hash)
+        self.block_object = BlockObject.parse(io.BytesIO(block_data))
+        self.block_height = 500809
+
+    # @patch('controllers.bitcoin.get_block')
+    def test_tx(self):
+        # mockedblock.return_value = self.block_object, self.block_height
+        block_checker()
+
+        # tx = self.block.txs[1]
+        self.assertEqual(1, 1)
+        # self.assertEqual(tx.w_id(), 'a705dac9b33a88d64fbe10f353a20051bdf8a717c84b981cb88591023c3e09ad')
